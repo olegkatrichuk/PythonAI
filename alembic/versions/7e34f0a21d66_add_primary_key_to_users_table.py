c205c9416987a3e156e7a465b2dd99ba9ad6a500
olegkatrichuk <katrichukoleg@gmail.com>
2025-07-13 13:41:12 +0300

    Fix: Add primary key to users table to fix foreign key constraint

---
+++ alembic/versions/7e34f0a21d66_add_primary_key_to_users_table.py
@@ -0,0 +1,38 @@
+"""add_primary_key_to_users_table
+
+Revision ID: 7e34f0a21d66
+Revises: d4dab894150b
+Create Date: 2025-07-13 13:34:48.282538
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '7e34f0a21d66'
+# 🔴 ИСПРАВЛЕНО: Эта миграция должна идти после d4dab894150b
+down_revision: Union[str, None] = 'd4dab894150b'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """
+    Добавляет ограничение первичного ключа (PRIMARY KEY) к столбцу `id`
+    в таблице `users`.
+    """
+    op.create_primary_key(
+        "pk_users",  # Имя для нового ограничения
+        "users",     # Имя таблицы
+        ["id"]       # Столбец, который станет первичным ключом
+    )
+
+
+def downgrade() -> None:
+    """
+    Удаляет ограничение первичного ключа с таблицы `users`.
+    """
+    op.drop_constraint("pk_users", "users", type_="primary")
--- alembic/versions/b91808cc20a1_add_analytics_tables_and_admin_role.py
+++ alembic/versions/b91808cc20a1_add_analytics_tables_and_admin_role.py
@@ -1,7 +1,7 @@
 """Add analytics tables and admin role
 
 Revision ID: b91808cc20a1
-Revises: d4dab894150b
+Revises: 7e34f0a21d66
 Create Date: 2025-07-13 09:37:11.284236
 
 """
@@ -13,7 +13,8 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'b91808cc20a1'
-down_revision: Union[str, None] = 'd4dab894150b'
+# 🔴 ИСПРАВЛЕНО: Эта миграция теперь зависит от миграции, добавляющей Primary Key
+down_revision: Union[str, None] = '7e34f0a21d66'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
